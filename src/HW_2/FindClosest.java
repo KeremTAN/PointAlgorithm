28703399ae8afe2d8f8bcd2c75a647b9e4d96f24
KeremTAN <keremtan.95@hotmail.com>
2021-09-11 03:07:11 +0300

    algorithm hw

---
+++ src/HW_2/FindClosest.java
@@ -0,0 +1,131 @@
+package HW_2;
+
+import java.awt.geom.Point2D;
+
+public class FindClosest {
+
+    private PointPair closestPointPair;
+    private final QuickSort quicksort = new QuickSort();
+
+    /**
+     * Constructor
+     *
+     * @param points --> point array
+     */
+    public FindClosest(Point2D.Double[] points) {
+        //Sort points by X coordinate
+        quicksort.sort(points, 0, points.length - 1, "compareX");
+        this.closestPointPair = calculateClosestPointPair(points, 0, points.length - 1);
+        //*********************************do nothing***************************************//
+    }
+
+    /**
+     * Get closest Point Pair
+     *
+     * @return closestPointPair
+     */
+    public PointPair getClosestPointPair() {
+        return this.closestPointPair;
+    }
+
+    /**
+     * Main method for calculate and return closest point pair
+     *
+     * @param p          --> point array
+     * @param startIndex --> First index of p[]
+     * @param lastIndex  --> last index of p[]
+     * @return
+     */
+    private PointPair calculateClosestPointPair(Point2D.Double[] p, int startIndex, int lastIndex) {
+        PointPair ret, temp;
+        /** FFHJSGFŞLHFŞHLKFHJNÇGFKNHGFŞNKMGHLN GKHNMG HNKGHLNMGHLGKH */
+        int index;
+        ret = getClosestPointPair(p[startIndex], p[startIndex + 1], p[startIndex + 2]);
+        /**  Divide-and-Conquer strategy.*/
+        if (lastIndex - startIndex > 3) {
+            index = (lastIndex + startIndex) / 2;
+            temp = calculateClosestPointPair(p, startIndex, index - 1);
+            ret = ret.getDistance() < temp.getDistance() ? ret : temp;
+            ret = stripClosest(p, (index - 1 - startIndex), ret);
+            //--------------------------------------------------------------------
+            temp = calculateClosestPointPair(p, index, lastIndex);
+            ret = ret.getDistance() < temp.getDistance() ? ret : temp;
+            ret = stripClosest(p, (lastIndex - index), ret);
+        }
+        /** ----------------------------- */
+        else if (lastIndex - startIndex == 3) {
+            temp = getClosestPointPair(p[startIndex], p[startIndex + 1], p[lastIndex]);
+            ret = ret.getDistance() < temp.getDistance() ? ret : temp;
+            ret = stripClosest(p, lastIndex - startIndex, ret);
+        }
+        else {
+            double maxDistance = Double.MAX_VALUE;
+            Point2D.Double p1 = new Point2D.Double(Double.MAX_VALUE, Double.MAX_VALUE);
+            Point2D.Double p2 = new Point2D.Double(0, 0);
+            ret = new PointPair(p1, p2);
+            for (int i = 0; i < lastIndex; i++) {
+                PointPair pointPair = new PointPair(p[i], p[i + 1]);
+                if (maxDistance > pointPair.getDistance()) {
+                    maxDistance = pointPair.getDistance();
+                    ret = pointPair;
+                }
+            }
+            temp = stripClosest(p, lastIndex - startIndex, ret);
+            return ret.getDistance() < temp.getDistance() ? ret : temp;
+        }
+
+
+        return ret;
+    }
+
+    /**
+     * calculate and return closest point pair from 3 points
+     *
+     * @param p1 --> point 1
+     * @param p2 --> point 2
+     * @param p3 --> point 3
+     * @return
+     */
+    private PointPair getClosestPointPair(Point2D.Double p1, Point2D.Double p2, Point2D.Double p3) {
+        PointPair coupled1 = new PointPair(p1, p2);
+        PointPair coupled2 = new PointPair(p2, p3);
+        PointPair coupled3 = new PointPair(p1, p3);
+        PointPair ret;
+        ret = getClosestPointPair(coupled1, coupled2);
+        ret = getClosestPointPair(coupled3, ret);
+        return ret;
+
+    }
+
+    private PointPair getClosestPointPair(PointPair p1, PointPair p2) {
+        if (p1.getDistance() < p2.getDistance())
+            return p1;
+        else return p2;
+    }
+
+    /**
+     * A utility function to find the distance between the closest points of
+     * strip of given size. All points in strip[] are sorted according to
+     * y coordinate. They all have an upper bound on minimum distance as d.
+     * Note that this method seems to be a O(n^2) method, but it's a O(n)
+     * method as the inner loop runs at most 6 times
+     *
+     * @param strip        --> point array
+     * @param size         --> strip array element count
+     * @param shortestLine --> shortest line calculated so far
+     * @return --> new shortest line
+     */
+    private PointPair stripClosest(Point2D.Double strip[], int size, PointPair shortestLine) {
+        PointPair pair;
+        quicksort.sort(strip, 0, size, "compareY");
+
+        for (int i = 0; i < size - 1; ++i)
+            if ((strip[i].getY() - strip[i + 1].getY()) < shortestLine.getDistance()) {
+                pair = new PointPair(strip[i], strip[i + 1]);
+                if (pair.getDistance() < shortestLine.getDistance())
+                    shortestLine = pair;
+            }
+        return shortestLine;
+    }
+
+}
---
+++ src/HW_2/QuickSort.java
@@ -0,0 +1,141 @@
+package HW_2;
+
+import java.awt.geom.Point2D;
+
+public class QuickSort {
+
+
+    /**
+     * Default Contructor
+     */
+    public QuickSort() {
+        //Empty constructor --- do nothing
+    }
+
+    /**
+     * The main function that implements QuickSort
+     *
+     * @param arr        --> Array to be sorted
+     * @param startIndex --> First index of arr[]
+     * @param lastIndex  --> Last index of arr[]
+     * @param orderBy    --> compareX or compareY
+     *                   compareX: sort minimum to maximum arr[] by X point
+     *                   compareY: sort minimum to maximum arr[] by Y point
+     */
+    public void sort(Point2D.Double[] arr, int startIndex, int lastIndex, String orderBy) {
+        int index;
+        /**  Divide-and-Conquer strategy.*/
+        if (lastIndex-startIndex>1) {
+            if (orderBy.equalsIgnoreCase("compareX")) {
+                index = partitionX(arr, startIndex, lastIndex);
+                sort(arr, startIndex, (index-1), "compareX");
+                sort(arr, (index), lastIndex, "compareX");
+            }
+            else if (orderBy.equalsIgnoreCase("compareY")) {
+                index = partitionY(arr, startIndex, lastIndex);
+                sort(arr, startIndex, (index-1), "compareY");
+                sort(arr, (index), lastIndex, "compareY");
+            }
+        }
+        /** ----------------------------- */
+    }
+
+    /**
+     * A utility function to swap two elements
+     *
+     * @param arr --> Array to be sorted
+     * @param i   --> first index
+     * @param j   --> second index
+     */
+    private void swap(Point2D.Double[] arr, int i, int j) {
+        Point2D.Double temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * Get Median of 3 order by Point.X
+     *
+     * @param arr   --> Array to be sorted
+     * @param left  --> First index of arr[]
+     * @param right --> Last index of arr[]
+     * @return
+     */
+    /**  Doğru çalışıyor */
+    private Point2D.Double getMedianX(Point2D.Double[] arr, int left, int right) {
+        int center = (left + right) / 2;
+        if (arr[left].x > arr[center].x) swap(arr, left, center);
+        if (arr[left].x > arr[right].x) swap(arr, left, right);
+        if (arr[center].x > arr[right].x) swap(arr, center, right);
+        swap(arr, center, (right - 1));
+        return arr[(right - 1)];
+    }
+
+    /**
+     * Get Median of 3 order by Point.Y
+     *
+     * @param arr   --> Array to be sorted
+     * @param left  --> First index of arr[]
+     * @param right --> Last index of arr[]
+     * @return
+     */
+    private Point2D.Double getMedianY(Point2D.Double[] arr, int left, int right) {
+        int center = (left + right) / 2;
+        if (arr[left].y > arr[center].y) swap(arr, left, center);
+        if (arr[left].y > arr[right].y) swap(arr, left, right);
+        if (arr[center].y > arr[right].y) swap(arr, center, right);
+        swap(arr, center, (right - 1));
+        return arr[(right - 1)];
+    }
+
+    /**
+     * This function takes median of three as pivot, places
+     * the pivot element at the end of the sorted
+     * array, and places all smaller (smaller than pivot)
+     * to left of pivot and all greater elements to right of pivot
+     * Sort order by Point.X
+     *
+     * @param arr        --> Array to be sorted
+     * @param startIndex --> First index of arr[]
+     * @param lastIndex  --> Last index of arr[]
+     * @return pivot index
+     */
+    private int partitionX(Point2D.Double[] arr, int startIndex, int lastIndex) {
+        Point2D.Double pivot = getMedianX(arr, startIndex, lastIndex);
+        int i = startIndex, j = lastIndex-1;
+        while (true) {
+            while (arr[++i].x < pivot.x) ;
+            while (arr[--j].x > pivot.x) ;
+            if (i < j) swap(arr, i, j);
+            else break;
+        }
+        swap(arr, i, lastIndex - 1);
+        return i;
+    }
+
+    /**
+     * This function takes median of three as pivot, places
+     * the pivot element at the end of the sorted
+     * array, and places all smaller (smaller than pivot)
+     * to left of pivot and all greater elements to right of pivot
+     * Sort order by Point.Y
+     *
+     * @param arr        --> Array to be sorted
+     * @param startIndex --> First index of arr[]
+     * @param lastIndex  --> Last index of arr[]
+     * @return pivot index
+     */
+    private int partitionY(Point2D.Double[] arr, int startIndex, int lastIndex) {
+        Point2D.Double pivot = getMedianY(arr, startIndex, lastIndex);
+        int i = startIndex, j = lastIndex-1;
+        while (true) {
+            while (arr[++i].y < pivot.y) ;
+            while (arr[--j].y > pivot.y) ;
+            if (i < j) swap(arr, i, j);
+            else break;
+        }
+        swap(arr, i, lastIndex - 1);
+        return i;
+    }
+
+}
